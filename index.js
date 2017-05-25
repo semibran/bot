2202c24b7b39698afd0aa653cc2a9b402bbfe010
Brandon Semilla <semibran@gmail.com>
2017-05-24 21:10:04 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const { runInNewContext: run } = require('vm')
+const { inspect } = require('util')
+const bench = require('bench')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const token = process.argv[2]
+const open = '```js'
+const close = '```'
+
+var options = { depth: 1, maxArrayLength: 10 }
+var initial = { bench, reset, console: { log } }
+var context = null
+var channel = null
+
+function reset() {
+	context = Object.assign({}, initial)
+}
+
+function log(value) {
+	channel.send(open + value + close)
+}
+
+reset()
+
+client.login(token)
+
+client.on('ready', _ => console.log(`logged in with token '${token}'`))
+
+client.on('message', message => {
+	if (message.author !== client.user) {
+		var string = message.content
+		if (string.startsWith(open) && string.endsWith(close)) {
+			var script = string.slice(open.length, string.length - close.length).trim()
+			var result = null
+
+			channel = message.channel
+
+			try {
+				result = run(script, context)
+			}
+
+			catch (error) {
+				log(`${error.name}: ${error.message}`)
+			}
+
+			if (result != null) {
+				log(inspect(result, options))
+			}
+		}
+	}
+})
---
+++ readme.md
@@ -0,0 +1,13 @@
+# bot
+> [REPL](https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop) bot for [Discord](https://discordapp.com)
+
+## setup
+After cloning this repository and installing dependencies, run `node . YOUR_CLIENT_TOKEN` to start the bot.
+
+## usage
+Once the bot is online, send a message with your code inside triple backticks flagged as JavaScript (````js`) to execute a script.
+
+The context exposes the methods `console.log`, `reset` (to reset the context to its initial state), and [`bench`](https://github.com/semibran/bench).
+
+## license
+MIT
